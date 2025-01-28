7a3646042b2796077e615d325bbf9865e9ce23ae
Ramji kumar <90134895+Ramjikumarcom@users.noreply.github.com>
2025-01-28 16:00:18 +0530

    Update github.c

--- github.c
+++ github.c
@@ -6,5 +6,6 @@ int main(){
     int d=a+b-c;
     printf("the vlaue of a and b and cis %d ",d);
     printf("this is final value of d");
+    printf("now it is update by the jenkins");
     return 0;
 }
